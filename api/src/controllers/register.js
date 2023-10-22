3a774981532bbfaee91e1dddf6a301d09448ed0f
ManuelContreras05 <josemanuelcontrerast018@gmail.com>
2023-10-22 16:31:40 -0500

    verificar si usuario ya existe

--- api/src/controllers/register.js
+++ api/src/controllers/register.js
@@ -1,6 +1,15 @@
-
 import db from "../database/index.js"
+import { checkUserExists } from './tu_ruta_al_controlador';
+
+
 const register = async (data, res) => {
+    try{
+      const userExists = await checkUserExists(data.user);
+
+      if(userExists)
+      {
+        return res.status(400).json({ message: 'El usuario ya existe' });
+      }
     db.query(`
         INSERT INTO usuarios (name, lastname, birthdate, user, password)
         VALUES ("${data.name}","${data.lastname}", "${data.birthdate}","${data.user}","${data.password}")`, (error, results) => {
@@ -11,6 +20,10 @@ const register = async (data, res) => {
         }
         return res.json(results);
       });
+    }catch(error){
+      console.error('Error: ', error)
+      res.status(500).send('Error interno del servidor');
+    }
 }
 
 export default register
